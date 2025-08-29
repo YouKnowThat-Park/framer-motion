107c512c578c96cf1d82719f1b18bbd9424a2f14
wooseok <youkn0wthat@naver.com>
2025-08-30 01:52:31 +0900

    style:  첫 화면에 보일 Hero 만듬 (initial, animate, transition 공부)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import React from "react";
+import Hero from "./hero/Hero";
 
-export default function Home() {
-  return <div></div>;
-}
+const page = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default page;
