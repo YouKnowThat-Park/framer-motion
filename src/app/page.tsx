7b6ba9986a4c563dd5d0026ea4f8e019d302e7d6
wooseok <youkn0wthat@naver.com>
2025-09-02 03:19:33 +0900

    🍻 chore: 만든 Next 컴포넌트 파일 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./hero/Hero";
+import Next from "./next/Next";
 
 const page = () => {
   return (
     <div>
       <Hero />
+      <Next />
     </div>
   );
 };
