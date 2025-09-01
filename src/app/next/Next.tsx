461acc4be6d7591328d45e7d9ec76fba5d9ed84e
wooseok <youkn0wthat@naver.com>
2025-09-02 03:15:01 +0900

    🍻 feat: Hero 다음 컴포넌트 파일 생성 (파일 명은 아직 정하지 못해서 Next로 해둠)

---
+++ src/app/next/Next.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Next = () => {
+  return (
+    <section className="h-[700px] w-full mb-10 bg-white flex items-center justify-center">
+      <div className="w-full text-[100px] font-bold text-black flex items-center justify-center">
+        Hello Mr. My Yesterday
+      </div>
+      ;
+    </section>
+  );
+};
+
+export default Next;
